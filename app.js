65b97a4b635bdfb42e312ebeacb6265f35624fb3
MrJP <salcedo.juanpa@gmail.com>
2022-04-24 19:46:35 -0700

    Complete backend using local DB

---
+++ app.js
@@ -0,0 +1,94 @@
+const express = require('express');
+const mysql = require('mysql');
+const cors = require('cors')
+const bodyParser = require('body-parser')
+
+const app = express();
+app.use(bodyParser.json())
+
+var corsOptions = {
+  origin: 'http://localhost:4200',
+  optionsSuccessStatus: 200
+}
+
+app.use(cors(corsOptions))
+
+//Create connection
+const conn = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'admin',
+  database: 'trackstarproducts'
+});
+ 
+//connect to database
+conn.connect((err) =>{
+  if(err) throw err;
+  console.log('Mysql Connected...');
+});
+
+app.route('/api').get((req, res) => {
+  let sql = "SELECT * FROM Products";
+  let query = conn.query(sql, (err, results) => {
+    if(err) throw err;
+    res.send(200, results)
+  });
+})
+
+app.route('/api/getproduct/:ProductID').get((req, res) => {
+  let sql = "SELECT * FROM Products WHERE ProductID="+req.params.ProductID+"";
+  let query = conn.query(sql, (err, results) => {
+    if(err) throw err;
+    res.send(200, results)
+  });
+})
+
+app.route('/api/getproductbyname/:Name').get((req, res) => {
+  let sql = "SELECT * FROM Products WHERE Name="+"'"+req.params.Name+"'"+"";
+  let query = conn.query(sql, (err, results) => {
+    if(err) throw err;
+    res.send(200, results)
+  });
+})
+
+
+
+app.route('/api/addproduct').post((req, res) => {
+  let data = {
+    ProductID: req.body.ProductID, 
+    Name: req.body.Name, 
+    Description: req.body.Description, 
+    Price: req.body.Price, 
+  };
+  let sql = "INSERT INTO Products SET ?";
+  let query = conn.query(sql, data,(err, results) => {
+    if(err) throw err;
+    res.send(201, req.body)
+  });
+})
+
+app.route('/api/updateproduct').put((req, res) => {
+  let data = {
+    ProductID: req.body.ProductID, 
+    Name: req.body.Name, 
+    Description: req.body.Description, 
+    Price: req.body.Price, 
+  };
+  let sql = "UPDATE Products SET Name='"+data.Name+"', Description='"+data.Description+"', Price='"+data.Price+"' WHERE ProductID="+data.ProductID;
+  let query = conn.query(sql, data,(err, results) => {
+    if(err) throw err;
+    res.send(200, req.body)
+  });
+})
+
+app.route('/api/removeproduct/:ProductID').delete((req, res) => {
+  let sql = "DELETE FROM Products WHERE ProductID="+req.params.ProductID+"";
+  let query = conn.query(sql, (err, results) => {
+    if(err) throw err;
+    res.sendStatus(204)
+  });
+})
+ 
+app.listen(8000, () => {
+  console.log('Server is running at port 8000');
+});
